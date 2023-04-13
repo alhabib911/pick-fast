aaef67f34f17124e053bc5feeb55babb13e0ea1b
alhabib911 <abdullahalhabib100@gmail.com>
2023-04-14 03:18:08 +0600

    Policy menu responsive done

--- src/Components/Common/BodyOne/BannerSection.js
+++ src/Components/Common/BodyOne/BannerSection.js
@@ -6,13 +6,15 @@ import sideBannerTwo from '../../../Asset/BodyOne/BannerSection/side-banner-two.
 const BannerSection = () => {
     return (
         <div>
-            <div className='flex gap-1 mx-28'>
-                <div className='w-2/3'>
+            <div className='flex lg:justify-start md:justify-end justify-center gap-1 lg:mx-28 md:mx-5 mx-0'>
+                <div className='lg:w-2/3 w-12/12'>
                     <img src={banner} alt="" />
                 </div>
-                <div className='w-2/6'>
-                    <img src={sideBannerOne} alt="" />
-                    <img src={sideBannerTwo} alt="" />
+                <div className='lg:w-2/6 lg:flex hidden'>
+                    <div>
+                        <img src={sideBannerOne} alt="" />
+                        <img src={sideBannerTwo} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
--- src/Components/Common/BodyOne/PolicyMenu.js
+++ src/Components/Common/BodyOne/PolicyMenu.js
@@ -5,8 +5,8 @@ import { SiQualcomm } from 'react-icons/si';
 
 const PolicyMenu = () => {
     return (
-        <div className='mx-28 pt-8 grid grid-cols-4 gap-5'>
-            <div className='flex justify-center py-4 rounded-md border border-gray-100 gap-2 bg-white'>
+        <div className='lg:mx-28 md:mx-6 lg:pt-8 md:pt-14 pt-2 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-5 gap-1'>
+            <div className='flex lg:justify-center md:justify-center justify-start lg:pl-0 md:pl-0 pl-24 py-4 rounded-md border border-gray-100 gap-2 bg-white'>
                 <div className='text-5xl text-[#F84B2F]'>
                     <TbTruckDelivery />
                 </div>
@@ -15,7 +15,7 @@ const PolicyMenu = () => {
                     <h3 className='text-sm'>When ordering over $100</h3>
                 </div>
             </div>
-            <div className='flex justify-center py-4 rounded-md border border-gray-100 gap-2 bg-white'>
+            <div className='flex lg:justify-center md:justify-center justify-start lg:pl-0 md:pl-0 pl-24 py-4 rounded-md border border-gray-100 gap-2 bg-white'>
                 <div className='text-5xl text-[#F84B2F]'>
                     <TbTruckReturn />
                 </div>
@@ -24,7 +24,7 @@ const PolicyMenu = () => {
                     <h3 className='text-sm'>7 Days return policy</h3>
                 </div>
             </div>
-            <div className='flex justify-center py-4 rounded-md border border-gray-100 gap-2 bg-white'>
+            <div className='flex lg:justify-center md:justify-center justify-start lg:pl-0 md:pl-0 pl-24 py-4 rounded-md border border-gray-100 gap-2 bg-white'>
                 <div className='text-4xl text-[#F84B2F]'>
                     <SiQualcomm />
                 </div>
@@ -33,7 +33,7 @@ const PolicyMenu = () => {
                     <h3 className='text-sm'>100+ Brand shop product</h3>
                 </div>
             </div>
-            <div className='flex justify-center py-4 rounded-md border border-gray-100 gap-2 bg-white'>
+            <div className='flex lg:justify-center md:justify-center justify-start lg:pl-0 md:pl-0 pl-24 py-4 rounded-md border border-gray-100 gap-2 bg-white'>
                 <div className='text-5xl text-[#F84B2F]'>
                     <TbReportMoney />
                 </div>
